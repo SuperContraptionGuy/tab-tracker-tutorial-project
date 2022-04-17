988c9376f04c4820eee5bc7cd2b48d90cf68eb70
Hudson Kendall <supercontraptionguy@gmail.com>
2022-04-16 21:05:51 -0600

    setup of express.js server

---
+++ server/src/app.js
@@ -0,0 +1,35 @@
+
+console.log('hello world.');
+
+const express = require('express');
+// process JSON data easily
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// basic webserver that enables us to build REST style API endpoints
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+
+// allows any client or host to access this from different domains, security
+// risk???  TODO security risk?
+app.use(cors());
+
+
+// defining a route in express (API endpoint to hit)
+//
+// kinds of requests:
+// get
+// post
+// put
+// patch
+// delete
+
+app.get('/status', (req, res) => {
+  res.send({ // send back json response
+    message: 'yo wad up world!',
+  });
+});
+
+app.listen(process.env.PORT || 8081);
